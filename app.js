8c65c311c9b8de9e661178a1a803e5f4f4f66837
DavidCs9 <davidcastrosi@hotmail.com>
2024-06-29 14:09:00 -0600

    chore: Add CORS configuration for frontend integration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const passwordRoutes = require("./routes/passwordRoutes");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -10,6 +11,16 @@ const app = express();
 
 // Conectar a la base de datos
 connectDB();
+
+// Middleware para parsear el body
+app.use(express.json());
+
+// Configurar CORS
+const corsOptions = {
+  origin: "http://localhost:4200", // Reemplaza con el origen de tu frontend
+  credentials: true, // Permite el uso de cookies
+};
+app.use(cors(corsOptions));
 
 // Middleware para parsear el body
 app.use(express.json());
